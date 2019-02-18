fe256c350c39de7eae0d911a36f3b4bb882ef864
Yannick Foeillet <bzhtux@gmail.com>
2019-02-18 15:42:24 +0100

    feat(step2): create jumpbox

---
+++ terraform/gcp/modules/jumpbox/instance.tf
@@ -0,0 +1,32 @@
+resource "google_compute_address" "jumpbox" {
+  name         = "jbx-public-ip"
+  address_type = "EXTERNAL"
+}
+
+resource "google_compute_instance" "jumpbox" {
+  name          = "${var.env_name}-jbx"
+  machine_type  = "${var.jumpbox_machine_type}"
+  zone          = "${element(var.zones, 1)}"
+  tags          = ["${var.env_name}-jumpbox-external"]
+
+  boot_disk {
+    initialize_params {
+      # image = "debian-cloud/debian-9"
+      image = "projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20190212a"
+      size  = "50"
+      type  = "pd-ssd"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${var.subnet}"
+
+    access_config {
+      nat_ip = "${google_compute_address.jumpbox.address}"
+    }
+  }
+
+  service_account {
+    scopes = ["cloud-platform"]
+  }
+}
