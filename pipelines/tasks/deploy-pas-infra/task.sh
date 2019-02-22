81efa3527ac244980ff466c8fbb5b89093aae71c
Yannick Foeillet <bzhtux@gmail.com>
2019-02-22 11:30:22 +0100

    fix(task): fix terraforming task

--- pipelines/tasks/deploy-pas-infra/task.sh
+++ pipelines/tasks/deploy-pas-infra/task.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
-cp -a terraform-tfvars/*.tfvars terraform-pivotalcf/terraforming-pas/terraform.tfvars
-cat terraform-pivotalcf/terraforming-pas/terraform.tfvars
+cp -a terraform-pivotalcf/* tf-src/
+cp -a terraform-tfvars/*.tfvars tf-src/terraforming-pas/terraform.tfvars
+cat tf-src/terraforming-pas/terraform.tfvars
 # cp -a terraform-pivotalcf/terraforming-pas/vm.tags vm-tags/vm.tags
