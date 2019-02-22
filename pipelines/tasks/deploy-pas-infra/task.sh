8a53d7b0daf5fe0e8e509c5c114ecf288c4c4c49
Yannick Foeillet <bzhtux@gmail.com>
2019-02-22 11:38:35 +0100

    fix(task): debug tfvars

--- pipelines/tasks/deploy-pas-infra/task.sh
+++ pipelines/tasks/deploy-pas-infra/task.sh
@@ -4,5 +4,10 @@ set -euo pipefail
 
 cp -a terraform-pivotalcf/* tf-src/
 cp -a terraform-tfvars/*.tfvars tf-src/terraforming-pas/terraform.tfvars
-cat tf-src/terraforming-pas/terraform.tfvars
+if [ -f "tf-src/terraforming-pas/terraform.tfvars" ]
+then
+    echo "terraform.tfvars was successfully copied to tf-src/terraforming-pas/terraform.tfvars"
+else
+    echo "Something went wrong when copying terraform.tfvars to tf-src :("
+fi
 # cp -a terraform-pivotalcf/terraforming-pas/vm.tags vm-tags/vm.tags
