b1c048a450831255facd2d2189f7089d4652ce16
Yannick Foeillet <bzhtux@gmail.com>
2019-02-22 10:36:34 +0100

    fix(task): Remove tags part as it should be done after terraforming

--- pipelines/tasks/deploy-pas-infra/task.sh
+++ pipelines/tasks/deploy-pas-infra/task.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 cp -a terraform-tfvars/*.tfvars terraform-pivotalcf/terraforming-pas/terraform.tfvars
-cp -a terraform-pivotalcf/terraforming-pas/vm.tags vm-tags/vm.tags
+# cp -a terraform-pivotalcf/terraforming-pas/vm.tags vm-tags/vm.tags
